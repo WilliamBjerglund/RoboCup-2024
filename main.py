7affde80add051dc16e6bf6fc2406cc09fbdd3fc
zaddmc <117923377+zaddmc@users.noreply.github.com>
2024-09-05 20:40:15 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,59 @@
 #!/usr/bin/env pybricks-micropython
-#import packages
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
-from pybricks.parameters import Port, Direction
+from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.parameters import Port, Color
 from pybricks.robotics import DriveBase
+from pybricks.tools import wait
+import threading
 
-# Initialize the EV3 Brick & everything else.
+def ToBe():
+    ev3.speaker.set_speech_options(speed=80)
+    ev3.speaker.set_volume(1000)
+    ev3.speaker.say("To be, or not to be, that is the question Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take arms against a sea of troubles And by opposing end them. To die—to sleep, No more; and by a sleep to say we end The heart-ache and the thousand natural shocks That flesh is heir to: 'tis a consummation Devoutly to be wish'd. To die, to sleep; To sleep, perchance to dream—ay, there's the rub: For in that sleep of death what dreams may come, When we have shuffled off this mortal coil, Must give us pause—there's the respect That makes calamity of so long life. For who would bear the whips and scorns of time,Th'oppressor's wrong, the proud man's contumely, The pangs of dispriz'd love, the law's delay, The insolence of office, and the spurns That patient merit of th'unworthy takes, When he himself might his quietus make With a bare bodkin? Who would fardels bear, To grunt and sweat under a weary life, But that the dread of something after death, The undiscovere'd country, from whose bourn No traveller returns, puzzles the will, And makes us rather bear those ills we have Than fly to others that we know not of? Thus conscience doth make cowards of us all, And thus the native hue of resolution Is sicklied o'er with the pale cast of thought, And enterprises of great pith and moment With this regard their currents turn awry And lose the name of action.")
+
+
+# Initialize the EV3 Brick.
 ev3 = EV3Brick()
+print("Hello")
+ev3.screen.print("Hello")
 
-# Initialize motors (left motor set to run counterclockwise)
-left_motor = Motor(Port.B)
-right_motor = Motor(Port.A)
-gripper_motor = Motor(Port.D)  # Make sure to use a different port for the arm motor
+#colSensor = ColorSensor(Port.S3)
+#while True:
+#    colData = colSensor.ambient()
+#    print(colData)
+#    ev3.screen.print(colData)
+#    if type(colData) == int or float:
+#        if colData > 20:
+#            ev3.speaker.say("of")
+#            ev3.speaker.play_file("roblox-death-sound_1.mp3")
+#    wait(100)
 
-# Initialize the drive base
-robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+spinny = Motor(Port.A)
 
-# Go forward and backwards for one meter.
-robot.straight(1000)
-ev3.speaker.beep()
 
-robot.straight(-1000)
-ev3.speaker.beep()
+#ev3.speaker.play_file("Oof.mp3")
+#ev3.speaker.say("To be, or not to be, that is the question Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take arms against a sea of troubles And by opposing end them. To die—to sleep, No more; and by a sleep to say we end The heart-ache and the thousand natural shocks That flesh is heir to: 'tis a consummation Devoutly to be wish'd. To die, to sleep; To sleep, perchance to dream—ay, there's the rub: For in that sleep of death what dreams may come, When we have shuffled off this mortal coil, Must give us pause—there's the respect That makes calamity of so long life. For who would bear the whips and scorns of time,Th'oppressor's wrong, the proud man's contumely, The pangs of dispriz'd love, the law's delay, The insolence of office, and the spurns That patient merit of th'unworthy takes, When he himself might his quietus make With a bare bodkin? Who would fardels bear, To grunt and sweat under a weary life, But that the dread of something after death, The undiscovere'd country, from whose bourn No traveller returns, puzzles the will, And makes us rather bear those ills we have Than fly to others that we know not of? Thus conscience doth make cowards of us all, And thus the native hue of resolution Is sicklied o'er with the pale cast of thought, And enterprises of great pith and moment With this regard their currents turn awry And lose the name of action.")
+#_thread.start_new_thread(ev3.speaker.say("To be, or not to be, that is the question Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take arms against a sea of troubles And by opposing end them. To die—to sleep, No more; and by a sleep to say we end The heart-ache and the thousand natural shocks That flesh is heir to: 'tis a consummation Devoutly to be wish'd. To die, to sleep; To sleep, perchance to dream—ay, there's the rub: For in that sleep of death what dreams may come, When we have shuffled off this mortal coil, Must give us pause—there's the respect That makes calamity of so long life. For who would bear the whips and scorns of time,Th'oppressor's wrong, the proud man's contumely, The pangs of dispriz'd love, the law's delay, The insolence of office, and the spurns That patient merit of th'unworthy takes, When he himself might his quietus make With a bare bodkin? Who would fardels bear, To grunt and sweat under a weary life, But that the dread of something after death, The undiscovere'd country, from whose bourn No traveller returns, puzzles the will, And makes us rather bear those ills we have Than fly to others that we know not of? Thus conscience doth make cowards of us all, And thus the native hue of resolution Is sicklied o'er with the pale cast of thought, And enterprises of great pith and moment With this regard their currents turn awry And lose the name of action."))
 
-# Turn clockwise by 360 degrees and back again.
-robot.turn(360)
-ev3.speaker.beep()
+t1 = threading.Thread(target=ToBe)
+t1.start()
 
-robot.turn(-360)
-ev3.speaker.beep()
+print("hello again")
+while True:
+    ev3.screen.print("yes")
+    spinny.run(200)
+    wait(4000)
+    spinny.stop()
 
-# Operate the gripper motor
-gripper_motor.run_target(75, 300)  # Adjust speed and angle as necessary
-gripper_motor.run_target(75, -300)
-ev3.speaker.beep()  # Beep to confirm completion
+    spinny.run(-200)
+    wait(4000)
+    spinny.stop()
+
+
+""" while True:
+    ev3.speaker.play_file("Oof - Roblox - QuickSounds.com.mp3")
+ """
+
+
+
+    
