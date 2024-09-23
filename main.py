5f473e7daa2f4da42ebffb720a8a08e5c9ba2c06
Boltz333 <113584616+Boltz333@users.noreply.github.com>
2024-09-23 11:48:31 +0200

    comitment issues

--- main.py
+++ main.py
@@ -16,130 +16,54 @@ Gyrosensor = GyroSensor(Port.S4)
 Ucensor = UltrasonicSensor(Port.S3)
 
 
-FfDistance = 100
+
+FfDistance = 50
 Dspeed = -180
 GENERAL_TURN = 18
 GYRO_THRESHOLD = 20
+grip_is_open = True
+GRIP_WAIT_TIME = 2500
 
 DBase=DriveBase(Motor_R, Motor_L, wheel_diameter=56, axle_track=100)
 
-def calibrate():
-    """
-    Calibrates the value to determine where the edge of the line begins, to correct path
-    Returns target value of the edge as an int
-    """
-    EV3.speaker.beep()
-    print("Starting calibration")
-    wait(1000)
-    target_val = Colorsensor.reflection()
-    EV3.speaker.beep()
-    print("Calibration: ", target_val)
-    return target_val
+
+
+
+
+def Gripper_debug(grip_is_open):
+    if grip_is_open:
     
-def follow_line(sign=1,turn_direction='straight'):
-    """ 
-    Follows line edge
-    Input defines which edge of the line to follow, where 1 (Default) is the right side and -1 is left side
-    Returns void
-    """
-   # if abs(sign) == 1:
-    #    raise Exception(f"follow_line got to big input value: '{sign}', limit it to '1' or '-1'")
-    
-    speed_multiplier = 1.15
-    local_turn_rate = 45
-    local_drive_speed = Dspeed
-    while True:
-        # Get new sensor data
-        current_val = Colorsensor.reflection()
-        gyro_angle = Gyrosensor.angle()
-        # Determine if black line has been reached, and thus next stage
-        if current_val < 30:
-            Gyrosensor.reset_angle(0)
-            DBase.stop()
-            break
-        
-        # Determine if in a turn and increase turn rate to drive faster without failing
-        if abs(gyro_angle) > GYRO_THRESHOLD:
-            #print("It hit me")
-            #local_turn_rate = 45
-            local_drive_speed *= speed_multiplier
-        else:
-            #local_turn_rate = GENERAL_TURN
-            local_drive_speed = Dspeed
-            
-        # Determine the amount of correction to stay on path
-        if target_val > current_val:
-            local_error = (target_val - current_val)/2
-        else:
-            local_error = (current_val - target_val)/2
-            
-        # Apply correction in drive module and which edge of the line to follow
-        if current_val < target_val:
-            if turn_direction == 'right' and abs(gyro_angle) > GYRO_THRESHOLD:
-                print("number 1")
-                DBase.drive(Dspeed * speed_multiplier, -sign * (local_turn_rate + local_error)) # right
-            else:
-                print("number 2")
-                DBase.drive(Dspeed, -sign * (GENERAL_TURN + local_error)) # right
-        if current_val > target_val:
-            if turn_direction == 'left' and abs(gyro_angle) > GYRO_THRESHOLD:
-                print("number 3")
-                DBase.drive(Dspeed * speed_multiplier, sign * (local_turn_rate + local_error)) # left
-            else:
-                print("number 4")
-                DBase.drive(Dspeed, sign * (GENERAL_TURN + local_error)) # left
-            
-        # Debug
-        #print(current_val)
-        #print(gyro_angle)
-    return
+        Motor_Grip.run(300)
+        wait(GRIP_WAIT_TIME)
+        grip_is_open = False
+    else:
+            Motor_Grip.run(-300)
+            wait(GRIP_WAIT_TIME)
+            grip_is_open = True
+    return grip_is_open
 
-def Fflaske():
-    while True:
-        print("entered")
-        print(Ucensor.distance())
-        if Ucensor.distance() <= FfDistance:
-            Motor_Grip.run(200)
-            wait(4000)
-            Motor_Grip.stop()
-            DBase.straight(120)
-            Motor_Grip.run(-200)
-            wait(4000)
-            Motor_Grip.stop()
-        if Ucensor.distance() > FfDistance:
-            print("exited")
-            break
-    return
+grip_is_open = Gripper_debug(grip_is_open)
+
+grip_is_open = Gripper_debug(grip_is_open)
+
+# def Fflaske():
+#     while True:
+#         print("entered")
+#         print(Ucensor.distance())
+#         if Ucensor.distance() <= FfDistance:
+#             Motor_Grip.run(200)
+#             wait(4000)
+#             Motor_Grip.stop()
+#             DBase.straight(120)
+#             Motor_Grip.run(-200)
+#             wait(4000)
+#             Motor_Grip.stop()
+#         if Ucensor.distance() > FfDistance:
+#             print("exited")
+#             break
+#     return
 
 
-# Setup
-target_val = calibrate()
-
-# First challagne
-if False:
-    follow_line(-1)
-    wait(300)
-    DBase.straight(-10)
-    DBase.turn(30)
-    while Colorsensor.reflection() > 50: # Første sving
-        DBase.drive(Dspeed, 0)
-    follow_line(1)
-    wait(300)
-    DBase.straight(-10)
-    DBase.turn(-30)
-    while Colorsensor.reflection() > 50: # Andet sving
-        DBase.drive(Dspeed, 0)
-
-# First turn 
-follow_line(-1, 'right')
-wait(300)
-
-# Grab Bottle
-DBase.straight(-255)
-DBase.turn(-120)
-DBase.straight(360)
-
-Fflaske()
 
 
-DBase.straight(-500)
+
