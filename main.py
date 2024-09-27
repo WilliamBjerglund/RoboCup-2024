b2e3a8422fb78ef8dd94b60e3f39fc5c187b7c95
Boltz333 <113584616+Boltz333@users.noreply.github.com>
2024-09-27 21:29:03 +0200

    the day is long and you are ugly

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def follow_line(sign=1, time=0, precision=0):
         drive_speed = DRIVE_SPEED
 
         if precision != 0:
-            turn_rate = local_error * 0.8 
+            turn_rate = local_error #* 0.8 
             drive_speed = -50
 
         if current_val < target_val:
@@ -104,7 +104,7 @@ def follow_line(sign=1, time=0, precision=0):
 
     return
 
-def move_bottle(drive_for:int): # Renamed, sorry patrik, it had a shit name
+def move_bottle(drive_for:int, bullseye=False): # Renamed, sorry patrik, it had a shit name
     """
     Will go straight until, presumably, a bottle is within threshold, after which it will lift up the bottle and drive given distance
     Input the driving distance after it has grabbed the bottle
@@ -118,12 +118,17 @@ def move_bottle(drive_for:int): # Renamed, sorry patrik, it had a shit name
     while True:
         current_reading = Ucensor.distance() 
         print("The distance to presumed bottle: " + str(Ucensor.distance()))
-        if  current_reading <= distance_threshold or counter >= 5 or counter_clocks >= 18:
-            DBase.straight(10)
+        #if  current_reading <= distance_threshold or counter >= 3 or counter_clocks >= 5:
+        if True: # it has ben changed as the new lineup is better, it therefore is hard coded to just pickup, hence it doesnt need to creep up
+            DBase.straight(7)
             Motor_Grip.run(200)
             wait(4000)
             Motor_Grip.stop()
             DBase.straight(drive_for)
+
+            if bullseye:
+                DBase.turn(-10)
+            
             Motor_Grip.run(-200)
             wait(4000)
             Motor_Grip.stop()
@@ -139,37 +144,66 @@ def move_bottle(drive_for:int): # Renamed, sorry patrik, it had a shit name
 
         last_reading = current_reading
         counter_clocks += 1
+            
+def scanning_lineup():
+    """
+    Will lineup with a given object infront of its ultrasonic sensor, does not work if there isn't a relatively tall object infront
+    """
+    # Various variables
+    count = 0
+    count_best_not_improved = 0
+    count_out_of_touch = 0
+    last_reading = 0
+    best = (count, 3000) # the first is index being rotation, the second is the value
 
-def lineup(angle:int = 0):
+    while True:
+        current_reading = Ucensor.distance()
+
+        # Evaluates best angle 
+        if current_reading <= best[1]:
+            best = (count, current_reading)
+
+        # getting the count of equivalent readings for estimating center of readings        
+        if current_reading == last_reading and best[1] == current_reading:
+            count_best_not_improved += 1
+
+        # Determining if the readings are too high and ready to exit
+        if current_reading > best[1] * 1.1:
+            count_out_of_touch += 1
+            if count_out_of_touch > 10:
+                break
+        #print("Distance reading " + str(current_reading) + " Count " + str(count))
+        
+        # Incrementable rotation and updates to variables
+        DBase.turn(1)
+        last_reading = current_reading
+        count += 1
+
+    # The angle to turn back to allign with the bottle
+    turn_back = (count - best[0] + count_best_not_improved/2)
+    DBase.turn(-turn_back)
+    #print("Total turned " + str(count) + " Determined turn back " + str(turn_back) + " Best reading " + str(best))
+    return
+
+def printnice(main='', secondary='', do_time=False):
     """
-    Depending on version it will attempt to lineup with a bottle or with a angle
+    Not particularly neccesary, but it is nice i suppose
     """
-    # This is the ultrasonic scanner version
-    if True:
-        correction = 0
-        distancethreshold = 450
-        while True:
-            reading = Ucensor.distance()
-            print('The correction value is: ' + str(correction) + ' and the ultrasensor is: ' + str(reading))
-            if reading < distancethreshold:
-                if correction >= 20:
-                    DBase.turn(15)
-                    return
-                    
-            else:
-                DBase.turn(1)
-            
-            if reading < distancethreshold:
-                correction += 1
-            else:
-                correction = 0
-            wait(10)
-            
+    if do_time:
+        print("Time progress: " + str((start_time_watch.time() - start_time)/60000) + "min")
+    if main != '':
+        print('-'*5 + main + '-'*5)
+    if secondary != '':
+        print('-' + secondary + '-')        
+    print()
+    return
 
 # Setup
 target_val = calibrate()
+
 current_settings = DBase.settings()
 DBase.settings(current_settings[0] + 50, current_settings[1], current_settings[2], current_settings[3])
+
 start_time_watch = StopWatch()
 start_time = start_time_watch.time()
 
@@ -177,6 +211,7 @@ start_time = start_time_watch.time()
 # Are the wait statements neccesary, i dont feel like they are but idk
 # The if statements with hard coded bools in them may seem unnessecary, because they are, but they make it easy to change where the robot can be placed to start, so that it doesnt have to run the whole route to test one thing
 if True:
+    printnice("Lane Switch")
     follow_line(-1)
     wait(300)
     DBase.straight(-10)
@@ -195,17 +230,24 @@ if True:
     while Colorsensor.reflection() > 50: 
         DBase.drive(DRIVE_SPEED, 0)
 
+
+if True:
 # First turn 
+    printnice("First Turn", do_time=True)
     follow_line(-1)
     wait(300)
 
 # Grab Bottle and move over line
-if True:
+    printnice("Grab Bottle", do_time=True)
+    print("-Preparing for lineup")
     DBase.straight(-250) 
-    DBase.turn(-140)
-    #DBase.straight(150)
-    lineup()
-#    DBase.straight(250)
+    DBase.turn(-105)
+    DBase.straight(150)
+    DBase.turn(-60)
+
+    print("-Acutally lining up")
+    scanning_lineup()
+    DBase.straight(235)
 
     move_bottle(250)
 
@@ -239,24 +281,45 @@ if True:
     wait(300)
 
     DBase.turn(-38)
-    DBase.straight(-340)
-    DBase.turn(28)
-
-    #DBase.turn(-25)
-    follow_line(1)
-
+    DBase.straight(-330)
+    DBase.turn(32)
 
     # Bulls eye bottle 
+if True:
+    follow_line(1)
+
+    DBase.straight(-300)
+    DBase.turn(-100)
+    follow_line(-1, precision=199)
+
+    DBase.straight(-800)
+    DBase.turn(205) # bigger goes to the right
+    DBase.straight(317)
+    move_bottle(-570, True)
+
+    DBase.drive(-200, 50)
+    wait(1000)
+    while Colorsensor.reflection() > 50:
+        DBase.drive(-200, 20)
+    
+    DBase.straight(-150)
+    DBase.turn(-90)
+    follow_line(1)
 
     wait(300)
     follow_line()
-
 
     #Rundt om flaske 1
     DBase.turn(60)
     follow_line(-1, time=2700)
     wait(300)
     DBase.turn(-20)
+
+    DBase.turn(60)
+    DBase.drive(200, -30)
+    while Colorsensor.reflection() > 50:
+        print()
+
     follow_line(-1, time=8500)
 
 # Black wall v2
@@ -299,4 +362,4 @@ if True:
     DBase.turn(100)
 
 
-print("It took " + str(start_time_watch.time() - start_time) + "ms")
+print("It took " + str((start_time_watch.time() - start_time)/60000) + "min")
